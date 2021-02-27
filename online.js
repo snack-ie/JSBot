89a82dea79e9377fe0d08d34757da02393e6440f
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-02-27 13:12:27 -0300

    Update online.js

--- online.js
+++ online.js
@@ -1,1 +1,12 @@
-
+const express = require('express');
+const app = express();
+ 
+app.get('/', function (req, res) {
+/*
+* You can change the text to make a cool easter egg
+* Remember to use UptimerRobot on the url
+*/
+  res.send('easter egg :)');
+});
+ 
+app.listen(3000);
