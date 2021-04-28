f3dc969e33a7f2ded50ade1a936171e124009934
Pietro <>
2021-04-28 00:46:41 +0000

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("./online");
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on("ready", () => {
-let { username, discriminator, id } = client.user
+let { username, discriminator, id } = client.user;
 console.log(`Up and running as ${username}#${discriminator} (${id})!\nInvite me using this link: https://discord.com/oauth2/authorize?client_id=${id}&permissions=8&scope=bot`);
 });
 
@@ -24,6 +25,19 @@ client.on('message', message => {
         commandFile.run(client, message, args);
     } catch (err) {
     console.log(err);
+    let date_ob = new Date();
+
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+
+    fs.writeFile('./errors/' + year + month + date + hours + minutes + seconds + '.txt', err.stack, function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
   }
 });
 
